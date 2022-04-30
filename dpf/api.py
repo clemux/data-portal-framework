fbdbd9ba4f41d6ce88397348181cc1e047f3679c
Clément Schreiner <clement@mux.me>
2022-04-30 16:28:08 +0200

    api: [results] add metadata for documentation

--- dpf/api.py
+++ dpf/api.py
@@ -1,10 +1,9 @@
-from fastapi import FastAPI, Depends
+from dpf.db import schemas, crud
+from dpf.db.database import SessionLocal, Session
+from fastapi import FastAPI, Depends, Query
 from starlette.routing import Route, Mount
 from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
-
-from dpf.db import schemas, crud
-from dpf.db.database import SessionLocal, Session
 
 
 def get_db():
@@ -32,7 +31,10 @@ app = FastAPI(routes=[
 
 
 @app.get('/results', response_model=schemas.CircResult)
-async def get_results(code_dept: str, code_circ: str, db: Session = Depends(get_db)):
+async def get_results(
+        code_dept: str = Query(..., description="Numéro de département", example="67", min_length=2),
+        code_circ: str = Query(..., description="Numéro de circonscription", example="02", min_length=2),
+        db: Session = Depends(get_db)):
     votes = crud.get_circ_results(db, code_dept, code_circ)
 
     return votes
